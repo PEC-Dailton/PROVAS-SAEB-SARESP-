33602c321f069beb43e4329728b0c9ebbe09ebd6
PEC-Dailton <150475445+PEC-Dailton@users.noreply.github.com>
2025-07-27 21:47:53 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,8 +5,8 @@
 
 
 async function gerenciarDiasProvas() {
-    let daysProva1 = parseInt(localStorage.getItem('daysProva1')) || 60;
-    let daysProva2 = parseInt(localStorage.getItem('daysProva2')) || 67;
+    let daysProva1 = parseInt(localStorage.getItem('daysProva1')) || 59;
+    let daysProva2 = parseInt(localStorage.getItem('daysProva2')) || 66;
 
     console.log("Dias Prova 1:", daysProva1);
     console.log("Dias Prova 2:", daysProva2);
