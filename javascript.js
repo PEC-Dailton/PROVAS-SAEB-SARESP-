65b8cb6386d28b5576f5325b62e6952616fec919
PEC-Dailton <150475445+PEC-Dailton@users.noreply.github.com>
2025-08-12 22:12:47 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -5,8 +5,8 @@
 
 
 async function gerenciarDiasProvas() {
-    let daysProva1 = parseInt(localStorage.getItem('daysProva1')) || 48;
-    let daysProva2 = parseInt(localStorage.getItem('daysProva2')) || 55;
+    let daysProva1 = parseInt(localStorage.getItem('daysProva1')) || 47;
+    let daysProva2 = parseInt(localStorage.getItem('daysProva2')) || 54;
 
     console.log("Dias Prova 1:", daysProva1);
     console.log("Dias Prova 2:", daysProva2);
@@ -67,6 +67,7 @@ setInterval(exibirDataHora, 1000);
 
 // Exibe a data e hora imediatamente
 exibirDataHora();
+
 
 
 
